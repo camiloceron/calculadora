a74ca9ffd491c848e6b44f12155ed9ca2ed514d3
camiloceron <ikamc014@gmail.com>
2015-02-24 16:21:19 -0500

    validaciones operaciones consecutivas

--- calculadora.js
+++ calculadora.js
@@ -1,7 +1,7 @@
 var resultado1,resultado2;
 var ban=0, r=0, operador=0,bandatos=0;
 function mostrarMensaje(x){	
-	if(ban==1 && operador!=6) //cuando hay un operador
+	if(ban==1) //cuando hay un operador
 	{
 		if(document.getElementById("result").value != "" && bandatos==0)
 		{
@@ -42,64 +42,51 @@ function operacion(oper){
 	bandatos=1;
 	document.getElementById("result").value = "";
 
-	if(oper!=10){operador = oper;}
+	if(oper!=10){
+		if(oper!=operador && operador!=0 && operador!=20 && oper!=5 && oper!=6 && oper!=7 && oper!=8 && oper!=9){operador=operador;}
+		else{operador=oper;}		
+	}
 
 	if(ban==1 || operador==6 || operador==5 || operador==7 || operador==8 || operador==9)
 	{
-		if(operador==1 && resultado2!= null)	{
-			r = parseFloat(resultado1+resultado2);
-			document.getElementById("result").value = r;
-		}		
-    	else if (operador==2 && resultado2!= null){
-			r = parseFloat(resultado1-resultado2);					
-			document.getElementById("result").value = r;	 		
-		}
-		else if (operador==3 && resultado2!= null){
-			r = parseFloat(resultado1*resultado2);			
-			document.getElementById("result").value = r;	 		
-		}
+		if(operador==1 && resultado2!= null){r = parseFloat(resultado1+resultado2);}		
+    	else if (operador==2 && resultado2!= null){r = parseFloat(resultado1-resultado2);}
+		else if (operador==3 && resultado2!= null){r = parseFloat(resultado1*resultado2);}
 		else if (operador==4 && resultado2!= null){
-			if(resultado2==0)
-			{
-				document.getElementById("result").value ="Error de division entre 0";
-			}
-			else{
-				r = parseFloat(resultado1/resultado2);					
-				document.getElementById("result").value = r;	 		
-			}	
+			 if(resultado2==0){document.getElementById("result").value ="Error de division entre 0";}
+			else{r = parseFloat(resultado1/resultado2);}	
 		}
-		else if(operador==5){//raiz cuadrada
-			r = parseFloat(Math.sqrt(resultado1));
-			document.getElementById("result").value = r;
+		else if(operador==5 && resultado1!=null && resultado2==null){//raiz cuadrada
+			r = Math.sqrt(resultado1);
 			operador=20;
-		}
+		} 
 		else if(operador==6 && resultado1!=null && resultado2==null){//x2			
-			r= parseFloat(resultado1*resultado1);
-			document.getElementById("result").value = r;
+			r= parseFloat(resultado1*resultado1);			
 			operador=20;
 		}
 		else if(operador==7 && resultado1!=null && resultado2==null){
-			r = Math.sin(resultado1);
-			document.getElementById("result").value = r;
+			r = Math.sin(resultado1);			
 			operador=20;
 		}
 		else if(operador==8 && resultado1!=null && resultado2==null){
-			r = Math.cos(resultado1);
-			document.getElementById("result").value = r;
+			r = Math.cos(resultado1);			
 			operador=20;
 		}
 		else if(operador==9 && resultado1!=null && resultado2==null){
-			r = Math.tan(resultado1);
-			document.getElementById("result").value = r;
+			r = Math.tan(resultado1);			
 			operador=20;
 		}
+		document.getElementById("result").value = r;
 		resultado1=r;
 		resultado2= null;			
 	}
 	if(operador==20){
-		ban=0;		
+		ban=0;				
 	}	
-	else{ban=1;}		
+	else{
+		ban=1;
+		operador=oper;
+	}		
   }
 }
 
